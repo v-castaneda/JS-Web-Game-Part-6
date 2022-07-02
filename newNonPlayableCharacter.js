adb5dad53dce56a0781aa829001e3083f028de17
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-07-02 16:35:45 -0500

    updates

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -34,19 +34,37 @@ function newNonPlayableCharacter(x, y) {
     });
   }
 
-  function walkNorth() {
-    direction = "north";
-    element.src = `./assets/red-character/north.gif`;
+  function walkNorth(time) {
+    return new Promise((resolve) => {
+      direction = "north";
+      element.src = `./assets/red-character/north.gif`;
+      setTimeout(() => {
+        stop();
+        resolve();
+      }, time);
+    });
   }
 
-  function walkWest() {
-    direction = "west";
-    element.src = `./assets/red-character/west.gif`;
+  function walkWest(time) {
+    return new Promise((resolve) => {
+      direction = "west";
+      element.src = `./assets/red-character/west.gif`;
+      setTimeout(() => {
+        stop();
+        resolve();
+      }, time);
+    });
   }
 
-  function walkSouth() {
-    direction = "south";
-    element.src = `./assets/red-character/south.gif`;
+  function walkSouth(time) {
+    return new Promise((resolve) => {
+      direction = "south";
+      element.src = `./assets/red-character/south.gif`;
+      setTimeout(() => {
+        stop();
+        resolve();
+      }, time);
+    });
   }
 
   function stop() {
